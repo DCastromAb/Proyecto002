4d0d5e15c21483eafd5ecc7a23d08c94b6374148
DCastromAb <david.castro@absidecorp.com>
2023-07-26 16:17:19 +0000

    Error Sintaxis

--- srv/cat-services.js
+++ srv/cat-services.js
@@ -1,11 +1,9 @@
 const cds = require('@sap/cds')
 module.exports = cds.service.impl(function () {
     const {Products} = this.entities()
-    
     this.after('each',Products, row =>{
         console.log(`Read Product: ${row.ID}`)
     })
-    
     this.after(['CREATE', 'UPDATE', 'DELETE'], [Products], async (Product, req) => {
         const header = req.data
         req.on('succeeded', () => {
